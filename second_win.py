4e864dcabd23760683e355607cd2628807b05a12
Rciman <kontilgomang510@gmail.com>
2026-05-25 21:01:56 +0700

    Update second_win.py

--- second_win.py
+++ second_win.py
@@ -1,1 +1,22 @@
+#second win part 2
+        self.l_line.addWidget(self.text_timer, alignment=Qt.AlignCenter)
+        self.l_line.addWidget(self.text_name, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.line_name, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text_age, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.line_age, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text_test1, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_test1, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text_test2, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_test2, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.text_test3, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_test3, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.line_test3, alignment=Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_next, alignment=Qt.AlignCenter)
+        self.h_line.addLayout(self.l_line)
+        self.h_line.addLayout(self.r_line)
+        self.setLayout(self.h_line)
 
+    
+
+    def connects(self):  # tambahkan ini bagian second win part 2
+        self.btn_next.clicked.connect(self.next_click)
