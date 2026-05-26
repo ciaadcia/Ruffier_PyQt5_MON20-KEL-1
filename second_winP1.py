a64723e5dfa8d01e3f46ca7f504a0688a1c7118e
User-salmanD <salmanofficialgmail@gmail.com>
2026-05-26 14:18:59 +0700

    Add files via upload

---
+++ second_winP1.py
@@ -0,0 +1,25 @@
+    def next_click(self):
+        self.hide()
+        self.fw = FinalWin()
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)    
+    def initUI(self):
+        self.btn_next = QPushButton(txt_sendresult, self)
+        self.btn_test1 = QPushButton(txt_starttest1, self)
+        self.btn_test2 = QPushButton(txt_starttest2, self)
+        self.btn_test3 = QPushButton(txt_starttest3, self)
+        self.text_name = QLabel(txt_name)
+        self.text_age = QLabel(txt_age)
+        self.text_test1 = QLabel(txt_test1)
+        self.text_test2 = QLabel(txt_test2)
+        self.text_test3 = QLabel(txt_test3)
+        self.text_timer = QLabel(txt_timer)
+        self.line_name = QLineEdit(txt_hintname)
+        self.line_age = QLineEdit(txt_hintage)
+        self.line_test1 = QLineEdit(txt_hinttest1)
+        self.line_test2 = QLineEdit(txt_hinttest2)
+        self.line_test3 = QLineEdit(txt_hinttest3)
